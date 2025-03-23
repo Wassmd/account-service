f9d24b8de2a7d01319b5e9f66ecd3aac1f790ae7
Wasim <mohammed.wasimuddin@rewe-group.com>
2025-03-23 03:34:10 +0100

    Set up keycloak authentication

--- src/main/java/com/paxier/GreetingResource.java
+++ src/main/java/com/paxier/GreetingResource.java
@@ -1,11 +1,12 @@
 package com.paxier;
 
+import io.quarkus.security.Authenticated;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
+@Authenticated
 public class GreetingResource {
 
     @GET
